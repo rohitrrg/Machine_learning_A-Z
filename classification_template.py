bcdcc39d62fbbcc28d31fc8db1998f887998d962
Rohit <rohitgadhwar05@gmail.com>
2019-08-05 08:58:48 +0530

    Classification

--- classification_template.py
+++ classification_template.py
@@ -45,7 +45,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('8. Logistic Regression (Training set)')
+plt.title('Classification (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -63,7 +63,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('8. Logistic Regression (Test set)')
+plt.title('Classification (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
